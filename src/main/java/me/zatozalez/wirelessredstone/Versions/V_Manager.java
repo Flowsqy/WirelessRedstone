51618936d86917fb94bfe1659d8f0ac3ab095feb
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2025-02-11 19:54:51 +0100

    Remove useless link to github repo in V_Manager

--- src/main/java/me/zatozalez/wirelessredstone/Versions/V_Manager.java
+++ src/main/java/me/zatozalez/wirelessredstone/Versions/V_Manager.java
@@ -10,7 +10,6 @@ public class V_Manager {
     public static String minecraftVersion = "0.0.0";
     public static String pluginVersion = "0.0.0";
     public static String latestPluginVersion = "0.0.0";
-    private static String api = "https://api.github.com/repos/ZatoZalez/WirelessRedstone/releases/latest";
     public static void setVersion(){
         String v = Bukkit.getVersion();
         v = v.substring(v.indexOf("(") + 1);
