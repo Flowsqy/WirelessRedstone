bdc135c3cbb4c050c859cab617006f2e094bb793
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2025-02-11 19:59:54 +0100

    Specify map types in M_Utility#placeHolder to avoid unsafe casts

--- src/main/java/me/zatozalez/wirelessredstone/Messages/M_Utility.java
+++ src/main/java/me/zatozalez/wirelessredstone/Messages/M_Utility.java
@@ -1,22 +1,28 @@
 package me.zatozalez.wirelessredstone.Messages;
 
-import me.zatozalez.wirelessredstone.Config.C_Value;
-import me.zatozalez.wirelessredstone.Redstone.DeviceType;
-import me.zatozalez.wirelessredstone.Utils.U_Device;
-import me.zatozalez.wirelessredstone.Utils.U_Log;
-import me.zatozalez.wirelessredstone.WirelessRedstone;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-import org.yaml.snakeyaml.Yaml;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
-import static java.nio.charset.StandardCharsets.ISO_8859_1;
-import static java.nio.charset.StandardCharsets.UTF_8;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import me.zatozalez.wirelessredstone.WirelessRedstone;
+import me.zatozalez.wirelessredstone.Config.C_Value;
+import me.zatozalez.wirelessredstone.Redstone.DeviceType;
+import me.zatozalez.wirelessredstone.Utils.U_Device;
+import me.zatozalez.wirelessredstone.Utils.U_Log;
 
 public class M_Utility {
     private static LinkedHashMap<String, String> defaultMessages = new LinkedHashMap<>();
@@ -24,9 +30,10 @@ public class M_Utility {
     private static final String messagesPath = WirelessRedstone.getPlugin().getDataFolder().getAbsolutePath();
     private static final String messagesFile = "messages.yml";
 
-    public static void initialize(){
+    public static void initialize() {
         File file = new File(messagesPath + "/" + messagesFile);
-        WirelessRedstone.Log(new U_Log(U_Log.LogType.INFORMATION, ChatColor.GRAY + "Default language is set as "+ ChatColor.RED + C_Value.getLanguage() + ChatColor.GRAY + "."));
+        WirelessRedstone.Log(new U_Log(U_Log.LogType.INFORMATION, ChatColor.GRAY + "Default language is set as "
+                + ChatColor.RED + C_Value.getLanguage() + ChatColor.GRAY + "."));
         String languageFile = "messages_" + C_Value.getLanguage() + ".yml";
 
         defaultMessages = readAndLoad(WirelessRedstone.getPlugin().getClass().getResourceAsStream("/" + languageFile));
@@ -46,29 +53,31 @@ public class M_Utility {
     private static void create() {
         try {
             String languageFile = "messages_" + C_Value.getLanguage() + ".yml";
-            byte[] buffer = WirelessRedstone.getPlugin().getClass().getResourceAsStream("/" + languageFile).readAllBytes();
+            byte[] buffer = WirelessRedstone.getPlugin().getClass().getResourceAsStream("/" + languageFile)
+                    .readAllBytes();
 
             File targetFile = new File(messagesPath + "/" + messagesFile);
             OutputStream outStream = new FileOutputStream(targetFile);
             outStream.write(buffer);
             outStream.close();
         } catch (IOException e) {
-            WirelessRedstone.Log(new U_Log(U_Log.LogType.ERROR, "Could not create " + messagesFile + ".\n" + e.getMessage()));
+            WirelessRedstone
+                    .Log(new U_Log(U_Log.LogType.ERROR, "Could not create " + messagesFile + ".\n" + e.getMessage()));
         }
     }
 
-    private static LinkedHashMap<String, String> readAndLoad(InputStream inputStream){
+    private static LinkedHashMap<String, String> readAndLoad(InputStream inputStream) {
         try {
             LinkedHashMap<String, String> values = new LinkedHashMap<>();
             BufferedReader reader = new BufferedReader(
                     new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-            while(reader.ready()) {
+            while (reader.ready()) {
                 String line = reader.readLine();
-                if(line.contains(":")) {
+                if (line.contains(":")) {
                     String[] l = line.split(":", 2);
                     String key = l[0].trim();
                     String value = l[1].trim();
-                    if(value.endsWith("\"") && value.startsWith("\"")) {
+                    if (value.endsWith("\"") && value.startsWith("\"")) {
                         value = value.substring(1, value.length() - 1);
                     }
                     values.put(key, value);
@@ -82,22 +91,21 @@ public class M_Utility {
         return null;
     }
 
-    private static void update(){
+    private static void update() {
         boolean update = false;
-        if(messages != null) {
+        if (messages != null) {
             for (String string : defaultMessages.keySet()) {
                 if (!messages.containsKey(string)) {
                     update = true;
                     messages.put(string, defaultMessages.get(string));
                 }
             }
-        }
-        else{
+        } else {
             create();
             return;
         }
 
-        if(update){
+        if (update) {
             try {
                 StringBuilder data = new StringBuilder();
                 File file = new File(messagesPath + "/" + messagesFile);
@@ -114,51 +122,53 @@ public class M_Utility {
                 writer.flush();
                 writer.close();
             } catch (Exception e) {
-                WirelessRedstone.Log(new U_Log(U_Log.LogType.ERROR, "Could not update " + messagesFile + ".\n" + e.getMessage()));
+                WirelessRedstone.Log(
+                        new U_Log(U_Log.LogType.ERROR, "Could not update " + messagesFile + ".\n" + e.getMessage()));
             }
         }
     }
 
-    public static String getMessage(String key){
+    public static String getMessage(String key) {
         return getMessage(key, null);
     }
 
-    public static String getMessage(String key, HashMap<String, Object> parameters){
+    public static String getMessage(String key, HashMap<String, Object> parameters) {
         String str = colorString(messages.get(key));
-        if(str == null)
+        if (str == null)
             str = colorString(defaultMessages.get(key));
-        if(str == null)
-            return ChatColor.RED + "Could not find message key: " + ChatColor.DARK_RED + key + ChatColor.RED + ". This is a Bug.";
-        if(parameters == null)
+        if (str == null)
+            return ChatColor.RED + "Could not find message key: " + ChatColor.DARK_RED + key + ChatColor.RED
+                    + ". This is a Bug.";
+        if (parameters == null)
             return str;
-        for(String k : parameters.keySet())
-            if(str.contains(k))
+        for (String k : parameters.keySet())
+            if (str.contains(k))
                 str = str.replace(k, parameters.get(k).toString());
         return str;
     }
 
-    public static boolean sendMessage(Player player, String value){
-        if(!C_Value.allowMessages())
+    public static boolean sendMessage(Player player, String value) {
+        if (!C_Value.allowMessages())
             return false;
-        if(player == null)
+        if (player == null)
             return false;
-        if(value.startsWith("!!"))
+        if (value.startsWith("!!"))
             return false;
 
         player.sendMessage(value);
         return true;
     }
 
-    public static HashMap placeHolder(Object ... parameters){
+    public static HashMap<String, Object> placeHolder(Object... parameters) {
         if (parameters == null || parameters.length % 2 != 0 || parameters.length == 0)
             return null;
 
         HashMap<String, Object> map = new HashMap<String, Object>();
         for (int i = 0; i < parameters.length; i++) {
-            String value = parameters[(i+1)].toString();
-            if(value.equals(DeviceType.RedstoneSender.toString()))
+            String value = parameters[(i + 1)].toString();
+            if (value.equals(DeviceType.RedstoneSender.toString()))
                 value = U_Device.getDeviceName(DeviceType.RedstoneSender);
-            else if(value.equals(DeviceType.RedstoneReceiver.toString()))
+            else if (value.equals(DeviceType.RedstoneReceiver.toString()))
                 value = U_Device.getDeviceName(DeviceType.RedstoneReceiver);
             map.put(parameters[i].toString(), value);
             i++;
@@ -166,8 +176,8 @@ public class M_Utility {
         return map;
     }
 
-    private static String colorString(String str){
-        if(str == null)
+    private static String colorString(String str) {
+        if (str == null)
             return null;
         return ChatColor.translateAlternateColorCodes('&', str);
     }
